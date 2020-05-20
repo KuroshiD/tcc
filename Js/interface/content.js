17b49dce8ec5b809879c4a4656649618e4f52b03
kuroshiD <gusanches601@gmail.com>
2020-05-20 16:36:08 -0300

    home

--- Js/interface/content.js
+++
@@ -1,8 +0,0 @@
-const foter = document.querySelector(".container-footer");
-
-    foter.style.backgroundImage = "url('../Imagens/gustavo.jpg')";
-    foter.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-    foter.style.backgroundPosition = "center";
-    foter.style.backgroundSize = "cover";
-    foter.style.backgroundRepeat = "no-repeat";
-    foter.style.backgroundBlendMode = "color-dodge";
